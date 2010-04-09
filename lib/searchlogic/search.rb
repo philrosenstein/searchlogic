f2700c7722fb46c9e5baa0558c5e2b70e7a1f8fb
Phil Rosenstein <phil.rosenstein@gmail.com>
2010-04-08 20:58:30 -0500

    support for date_select and datetime_select form helpers

--- lib/searchlogic/search.rb
+++ lib/searchlogic/search.rb
@@ -63,6 +63,19 @@ module Searchlogic
     # Accepts a hash of conditions.
     def conditions=(values)
       values.each do |condition, value|
+        if condition =~ /(.*)\(1i\)$/
+          date_scope_name = $1
+          date_parts = (1..6).to_a.map do |idx|
+            values.delete("#{ date_scope_name }(#{ idx }i)")
+          end.reject{|s| s.blank? }.map{|s| s.to_i }
+      
+          if date_parts.length == 3 || date_parts.length == 5
+            values[date_scope_name] = Time.zone.local(*date_parts)
+          end
+        end
+      end
+      
+      values.each do |condition, value|
         mass_conditions[condition.to_sym] = value
         value.delete_if { |v| ignore_value?(v) } if value.is_a?(Array)
         next if ignore_value?(value)
